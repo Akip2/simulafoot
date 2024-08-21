34573aa34adc9193aef0248affc06e90c5d1f1a9
Antoine FONTANEZ <antoinef252@gmail.com>
2024-08-21 15:53:25 +0200

    Page nos-actualites

--- js/actu.js
+++ js/actu.js
@@ -1,7 +1,45 @@
-const p4Template = document.querySelector('#p4-template').innerHTML;
+//import Handlebars from "handlebars";
 
+let template;
 
-let template = Handlebars.compile(p4Template);
-document.querySelector("section.main")
- .innerHTML = template( { prenom : "marcel"
-, age: 42} ) ;
+let jsonData;
+
+let actuContainer;
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    actuContainer=document.querySelector("#actu_container");
+
+    const actuPrevTemplate = document.querySelector('#actu-template').innerHTML;
+    template = Handlebars.compile(actuPrevTemplate);
+});
+
+async function init(){
+    await fetch('actu-details.json')
+    .then(async (response) => {
+        jsonData=await response.json();
+    });
+}
+
+export async function printAll(){
+    await init();
+
+    for(let i=jsonData.length-1; i>=0; i--){
+        let actu=jsonData[i];
+
+        actuContainer.innerHTML+=template(actu);
+    }
+}
+
+export async function printAllExcept(exception){
+    await init();
+
+    exception--;
+
+    for(let i=jsonData.length-1; i>=0; i--){
+        if(i!=exception){
+            let actu=jsonData[i];
+
+            actuContainer.innerHTML+=template(actu);
+        }
+    }
+}
